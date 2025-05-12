82f1109a4bf25e9177e0a240a2abbe2d5053b2bf
zohaibcs657 <2019cs657@student.uet.edu.pk>
2025-05-13 02:44:44 +0500

    Adding Styles in Main Div

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -2,11 +2,12 @@
 
 const HelloWorld = () => {
   return( 
-  <div>
+  <div style={{textAlign:"center"}}>
   <h1>Hello World</h1>
   <h2>My Name is ZOHAIB FAZAL KARIM</h2>
   <h2>I am a full stack developer</h2>
   <h2>Currently working at VISNEXT SOFTWARE COMPANY</h2>
+  <h2>I am student of UET LAHORE</h2>
   </div>
 )};
 
