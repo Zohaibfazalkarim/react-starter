a56a827891a9050bf708fb10485dc460098c5a89
zohaib <zohaibfazalkarim1@gmail.com>
2025-05-13 01:22:44 +0500

    feat: add HelloWorld component on homepage

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -6,6 +6,7 @@ const HelloWorld = () => {
   <h1>Hello World</h1>
   <h2>My Name is ZOHAIB FAZAL KARIM</h2>
   <h2>I am a full stack developer</h2>
+  <h2>Currently working at VISNEXT SOFTWARE COMPANY</h2>
   </div>
 )};
 
